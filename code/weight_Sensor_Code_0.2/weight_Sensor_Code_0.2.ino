451c1c6a21677a0cef22aebcd51fee250ec5806a
Tobi <adejumool@berea.edu>
2019-01-31 19:42:39 -0500

    Update code/weight_Sensor_Code_0.2/weight_Sensor_Code_0.2.ino

--- code/weight_Sensor_Code_0.2/weight_Sensor_Code_0.2.ino
+++ code/weight_Sensor_Code_0.2/weight_Sensor_Code_0.2.ino
@@ -1,5 +1,5 @@
 int buzzer = 7;
-int fsrPin = A0;
+int fsrPin = A2;
 unsigned long time;
 bool check = false;
 int num = 0;
@@ -14,15 +14,18 @@ void setup() {
 void loop() {
 
   int fsrVal = analogRead(fsrPin);
-  time = millis();  
+  time = millis();
   Serial.print(fsrVal);
-  if ((time >= num+1000)&& (fsrVal < 500)) {
-      tone (buzzer, 500);
-      check = true;
-  if ((time >= 60000)&& (fsrVal > 500) && (check = true)){
-      num = millis();
+  Serial.print('\n');
+    if ((time >= num + 5000) && (fsrVal > 100) && (check == false)) {
+    tone (buzzer, 500);
+    check = true;
+    }
+
+    // if weight sesnor is pressed it will read 0
+    if ((fsrVal == 0) && (check == true)) {
+      num = millis(); //increases the num count
+      noTone (buzzer);// stops buzzer
       check = false;
     }
   }
-    
-}
