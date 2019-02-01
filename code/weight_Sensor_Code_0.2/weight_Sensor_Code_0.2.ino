151712d5ef9025c6c3d7fa960f467fc355938eef
Tobi <adejumool@berea.edu>
2019-01-31 19:04:27 -0500

    Code and pictures

--- code/weight_Sensor_Code_0.2/weight_Sensor_Code_0.2.ino
+++ code/weight_Sensor_Code_0.2/weight_Sensor_Code_0.2.ino
@@ -1,4 +1,4 @@
-int pingPin = 7;
+int buzzer = 7;
 int fsrPin = A0;
 unsigned long time;
 bool check = false;
@@ -6,7 +6,7 @@ int num = 0;
 void setup() {
   // initialize serial communication:
   pinMode(fsrPin, INPUT);
-  pinMode(pingPin, OUTPUT);
+  pinMode(buzzer, OUTPUT);
   Serial.begin(9600);
 
 }
@@ -16,11 +16,11 @@ void loop() {
   int fsrVal = analogRead(fsrPin);
   time = millis();  
   Serial.print(fsrVal);
-  if ((time >= num+60000)&& (fsrVal < 500)) {
-      digitalWrite(pingPin, HIGH);
+  if ((time >= num+1000)&& (fsrVal < 500)) {
+      tone (buzzer, 500);
       check = true;
   if ((time >= 60000)&& (fsrVal > 500) && (check = true)){
-      num = millis();// need to add code to reset timer here
+      num = millis();
       check = false;
     }
   }
