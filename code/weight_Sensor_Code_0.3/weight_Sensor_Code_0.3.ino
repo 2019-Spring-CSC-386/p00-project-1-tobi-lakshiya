ade5c9bb6c029dcdd27077a3aa27eb9f4068dfb9
Tobi <adejumool@berea.edu>
2019-02-05 11:32:53 -0500

    Updated Code

--- code/weight_Sensor_Code_0.3/weight_Sensor_Code_0.3.ino
+++ code/weight_Sensor_Code_0.3/weight_Sensor_Code_0.3.ino
@@ -1,7 +1,8 @@
 int buzzer = 7;
 int fsrPin = A2;
 unsigned long StartTime = millis();
-unsigned long TimePassed = 0;
+unsigned long CheckTime = 5000;
+
 void setup() {
   // initialize serial communication:
   pinMode(fsrPin, INPUT);
@@ -14,19 +15,19 @@ void loop() {
   StartTime = millis();
   int fsrVal = analogRead(fsrPin);
   //Serial.print(fsrVal);
-  Serial.print('\n');
-  TimePassed = StartTime - TimePassed;
-  Serial.print(StartTime);
-  Serial.print("\n time passed");
-  Serial.print(TimePassed);
- 
-    if (TimePassed > 5000 && (fsrVal > 100)) {
+  //Serial.print('\n');
+  //Serial.print(StartTime);
+  //Serial.print("\n Check passed");
+  //Serial.print(CheckTime);
+
+    // if 5 seconds has passed and there is nothing on the weight sensor
+    if ((StartTime > CheckTime) &&  (StartTime < CheckTime+3000) && (fsrVal > 100)) {
     tone (buzzer, 500);
     }
 
     // if weight sesnor is pressed it will read 0
     if (fsrVal == 0) {
       noTone (buzzer);// stops buzzer
-      TimePassed = StartTime;
+      CheckTime = StartTime + 5000;
     }
   }
